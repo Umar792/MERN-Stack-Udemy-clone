abb1f52c65355b4bfc1237f67e9c4491b45b22b7
MBilalShafqat <106830172+Umar792@users.noreply.github.com>
2023-07-25 23:08:44 +0500

    new

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./component/Home/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
